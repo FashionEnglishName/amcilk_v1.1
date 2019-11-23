09198810a5a8ac0d89fed7c5a0df634e765a88bf
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-11-23 01:01:54 -0600

    zhe

--- runtime/container.c
+++ runtime/container.c
@@ -153,7 +153,7 @@ void container_block(__cilkrts_worker * w) {
 
             //activated
             w = __cilkrts_get_tls_worker();
-            usleep(1000);
+            usleep(10);
             pthread_mutex_lock(&(w->g->program->G->lock));
             //pthread_spin_lock(&(w->g->program->G->lock));
             //printf("[G LOCK]: %d GET the G_lock\n", w->g->program->control_uid);
