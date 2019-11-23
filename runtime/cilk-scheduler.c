c31627af8430be979d3093044ac4a2f4f015941f
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-11-22 22:32:28 -0600

    zhe

--- runtime/cilk-scheduler.c
+++ runtime/cilk-scheduler.c
@@ -1530,7 +1530,7 @@ normal_point: //normal part, can not be preempted
             w = __cilkrts_get_tls_worker();
             int victim = rts_rand(w) % w->g->elastic_core->ptr_sleeping_inactive_deque;
             int victim_worker_id = w->g->elastic_core->cpu_state_group[victim];
-            if(victim_worker_id != w->self && (w->g->workers[victim_worker_id]->l->elastic_s==ACTIVE || w->g->workers[victim_worker_id]->l->elastic_s==SLEEPING_ACTIVE_DEQUE)) {
+            if(victim_worker_id != w->self && (w->g->workers[victim_worker_id]->l->elastic_s==ACTIVE || w->g->workers[victim_worker_id]->l->elastic_s==SLEEPING_ACTIVE_DEQUE || w->g->workers[victim_worker_id]->l->elastic_s==SLEEP_REQUESTED)) {
                 t = Closure_steal(w, victim_worker_id);
             } else if (victim_worker_id != w->self) {
                 printf("s%d w%d, %d\n", w->self, victim_worker_id, w->g->workers[victim_worker_id]->l->elastic_s);
