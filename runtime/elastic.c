51bcd81c0b994ed37c04de23eb7553c3aba1b763
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-11-22 21:16:22 -0600

    zhe

--- runtime/elastic.c
+++ runtime/elastic.c
@@ -327,11 +327,11 @@ void platform_guarantee_sleep_worker(platform_program * p, int cpu_id) {
                 printf("\t (p:%d w:%d) guarantee %d worker %d is in ACTIVE state, set as SLEEP_REQUESTED\n", 
                     w->g->program->control_uid, w->self, p->control_uid, cpu_id);
             }
+            usleep(TIME_MAKE_SURE_TO_SLEEP);
             p->g->workers[cpu_id]->exc = p->g->workers[cpu_id]->tail + DEFAULT_DEQ_DEPTH; //invoke exception handler
             if (p->is_switching==0 && p->g->workers[cpu_id]->l->elastic_s!=DO_MUGGING) {
                 count++;
             }
-            usleep(TIME_MAKE_SURE_TO_SLEEP);
             if (count>GUARANTEE_PREMPT_WAITING_TIME) {
                 printf("ERROR! (p:%d w:%d) guarantee sleep failed in (p:%d w:%d last:%d job_finish:%d hint_stop_container:%d) in e state %d\n", 
                     w->g->program->control_uid, w->self, p->control_uid, cpu_id, p->last_do_exit_worker_id, p->job_finish, p->hint_stop_container, 
@@ -353,11 +353,11 @@ void platform_guarantee_sleep_inactive_deque_worker(platform_program * p, int cp
         Cilk_fence();
         while(p->g->workers[cpu_id]->l->elastic_s!=SLEEPING_INACTIVE_DEQUE) {
             __sync_bool_compare_and_swap(&(p->g->workers[cpu_id]->l->elastic_s), ACTIVE, SLEEP_REQUESTED);
+            usleep(TIME_MAKE_SURE_TO_SLEEP);
             p->g->workers[cpu_id]->exc = p->g->workers[cpu_id]->tail + DEFAULT_DEQ_DEPTH; //invoke exception handler
             if (p->g->workers[cpu_id]->l->elastic_s!=SLEEPING_MUGGING_DEQUE && p->g->workers[cpu_id]->l->elastic_s!=SLEEPING_ADAPTING_DEQUE) {
                 count++;
             }
-            usleep(TIME_MAKE_SURE_TO_SLEEP);
             if (count>GUARANTEE_PREMPT_WAITING_TIME) {
                 printf("ERROR! (p:%d w:%d) guarantee sleep_inactive_deque failed in (p:%d w:%d last:%d job_finish:%d hint_stop_container:%d) in e state %d)\n", 
                     w->g->program->control_uid, w->self, p->control_uid, cpu_id, p->last_do_exit_worker_id, p->job_finish, p->hint_stop_container, 
