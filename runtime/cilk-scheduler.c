e9dcc5424af161514c628a27a3f2fc4f1aeb1737
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-11-25 23:03:36 -0600

    zhe

--- runtime/cilk-scheduler.c
+++ runtime/cilk-scheduler.c
@@ -1080,7 +1080,7 @@ static Closure * do_what_it_says(__cilkrts_worker * w, Closure *t) {
                                         Closure *cl;
                                         cl = deque_xtract_bottom(w, w->self);
                                         if (cl!=NULL) {
-                                            Closure_lock(w, cl);
+                                            //Closure_lock(w, cl);
                                             if (cl->status==CLOSURE_RETURNING) { //give up mugging
                                                 w = __cilkrts_get_tls_worker();
                                                 cl = return_value(w, cl);
@@ -1091,7 +1091,7 @@ static Closure * do_what_it_says(__cilkrts_worker * w, Closure *t) {
                                                         if (__sync_bool_compare_and_swap(&(w->g->workers[victim]->l->elastic_s), SLEEPING_MUGGING_DEQUE, SLEEPING_ACTIVE_DEQUE)) {
                                                             if (__sync_bool_compare_and_swap(&(w->l->elastic_s), DO_MUGGING, ACTIVE)) {
                                                                 printf("p%d, w%d: GIVE UP MUGGING\n", w->g->program->control_uid, w->self);
-                                                                Closure_unlock(w, cl);
+                                                                //Closure_unlock(w, cl);
                                                                 deque_unlock_self(w);
                                                                 elastic_core_unlock(w);
                                                                 longjmp_to_user_code(w, cl);
@@ -1109,7 +1109,7 @@ static Closure * do_what_it_says(__cilkrts_worker * w, Closure *t) {
                                                 printf("ERROR: wrong cl status at bottom [%d] when mugging\n", cl->status);
                                                 abort();
                                             }
-                                            Closure_unlock(w, cl);
+                                            //Closure_unlock(w, cl);
                                         }
                                         deque_unlock_self(w);
                                         
@@ -1178,7 +1178,7 @@ static Closure * do_what_it_says(__cilkrts_worker * w, Closure *t) {
                                     deque_lock_self(w);
                                     Closure *cl = deque_peek_bottom(w, w->self);
                                     if (cl!=NULL) {
-                                        Closure_lock(w, cl);
+                                        //Closure_lock(w, cl);
                                         if (__sync_bool_compare_and_swap(&(w->l->elastic_s), ACTIVATE_REQUESTED, ACTIVATING)) {
                                             elastic_core_lock(w);
                                             elastic_do_exchange_state_group(w, w->g->workers[w->g->elastic_core->cpu_state_group[w->g->elastic_core->ptr_sleeping_active_deque]]);
@@ -1187,7 +1187,7 @@ static Closure * do_what_it_says(__cilkrts_worker * w, Closure *t) {
                                             if (__sync_bool_compare_and_swap(&(w->l->elastic_s), ACTIVATING, ACTIVE)) {
                                                 if (cl->status==CLOSURE_RUNNING) {
                                                     if (w->current_stack_frame!=NULL) {
-                                                        Closure_unlock(w, cl);
+                                                        //Closure_unlock(w, cl);
                                                         deque_unlock_self(w);
                                                         sysdep_longjmp_to_sf(w->current_stack_frame);
                                                     } else {
@@ -1206,7 +1206,7 @@ static Closure * do_what_it_says(__cilkrts_worker * w, Closure *t) {
                                             printf("ERROR: activated without requested2\n");
                                             abort();
                                         }
-                                        Closure_unlock(w, cl);
+                                        //Closure_unlock(w, cl);
                                         deque_unlock_self(w);
                                     } else { //be mugged
                                         if (__sync_bool_compare_and_swap(&(w->l->elastic_s), ACTIVATE_REQUESTED, ACTIVATING)) {
