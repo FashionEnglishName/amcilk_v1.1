c4aa89fab2dc25ff082b26dbf6dcc411aafbc810
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-12-09 15:56:17 -0600

    zhe

--- runtime/platform_sched_kunal_adapt.c
+++ runtime/platform_sched_kunal_adapt.c
@@ -1,6 +1,9 @@
 #include "platform_sched_kunal_adapt.h"
 #include "sched_stats.h"
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 void reset_cpu_cycle_status(platform_program * p) {
 	if (p->job_init_finish==1) {
 		int i = 0;
@@ -35,8 +38,8 @@ int analyze_cpu_cycle_status(platform_program * p) {
 			p->total_stealing_cycles*(1.0)/p->total_cycles,
 			p->total_work_cycles,
 			non_steal_utilization);*/
+
 		float non_steal_utilization = p->total_work_cycles*(1.0)/p->total_cycles;
-		
 		int efficient = 0;
 		int satisfied = 0;
 		if (non_steal_utilization < KUNAL_ADAPTIVE_FEEDBACK_INEFFICIENT_THRESHOLD) {
@@ -50,15 +53,19 @@ int analyze_cpu_cycle_status(platform_program * p) {
 			satisfied = 0;
 		} else {
 			printf("BUG, num_cpu > desired_num_cpu\n");
-			satisfied = 1;
-			//abort();
+			abort();
 		}
 		if (efficient==0) {
-			p->desired_num_cpu = p->desired_num_cpu/KUNAL_ADAPTIVE_FEEDBACK_RESPONSIVENESS_PARAMETER;
+			p->desired_num_cpu = MIN(p->desired_num_cpu/KUNAL_ADAPTIVE_FEEDBACK_RESPONSIVENESS_PARAMETER, 1);
 		} else if (satisfied==1) {
-			p->desired_num_cpu = p->desired_num_cpu*KUNAL_ADAPTIVE_FEEDBACK_RESPONSIVENESS_PARAMETER;
+			p->desired_num_cpu = MIN(p->desired_num_cpu*KUNAL_ADAPTIVE_FEEDBACK_RESPONSIVENESS_PARAMETER, 1);
 		} else {
 			p->desired_num_cpu = p->desired_num_cpu;
+		}
+
+		if (p->desired_num_cpu<=0) {
+			printf("BUG, desired_num_cpu <= 0\n");
+			abort();
 		}
 
 		printf("(p%d, job finish:%d, input:%d, cpu num:%d): non-steal: %f, efficient: %d, satisfied: %d, desired: %d\n", 
@@ -70,7 +77,6 @@ int analyze_cpu_cycle_status(platform_program * p) {
 			efficient,
 			satisfied,
 			p->desired_num_cpu);
-
 		return 0;
 	} else {
 		printf("kunal_adaptive_scheduler: analyze_cpu_cycle_status failed (not start running)\n");
@@ -78,13 +84,58 @@ int analyze_cpu_cycle_status(platform_program * p) {
 	}
 }
 
+void adjust_scheduling(platform_program * p) {
+	//adjust try_num_cpu and try_cpu_mask
+	int to_set_num_cpu = MIN(p->desired_num_cpu, p->try_num_cpu);
+	
+}
+
 void kunal_adaptive_scheduler(platform_global_state * G) {
-    //TODO
     printf("do kunal_adaptive_scheduler\n");
+    int i = 0;
     platform_program * tmp_p = G->program_head->next;
+    platform_program * tmp_p2 = G->program_head->next;
     while(tmp_p!=NULL) {
     	pthread_spin_lock(&(tmp_p->cpu_cycle_status_lock));
     	analyze_cpu_cycle_status(tmp_p);
+    	pthread_spin_unlock(&(tmp_p->cpu_cycle_status_lock));
+    	tmp_p = tmp_p->next;
+    }
+
+    //adjust cpu_mask: give up unnecessay cores
+    tmp_p = G->program_head->next;
+    while(tmp_p!=NULL) {
+    	if (tmp_p->desired_num_cpu<tmp_p->try_num_cpu) {
+    		int tmp_diff_num_cpu = tmp_p->try_num_cpu - tmp_p->desired_num_cpu;
+    		for (i=0; i<p->G->nproc; i++) {
+    			if (tmp_p->try_cpu_mask[i]==1 && tmp_diff_num_cpu>0) {
+    				tmp_p->try_cpu_mask[i] = 0;
+    				tmp_diff_num_cpu--;
+    			}
+    		}
+    		tmp_p->try_num_cpu = tmp_p->desired_num_cpu;
+    	}
+    	tmp_p = tmp_p->next;
+    }
+    //see idle cores
+    for (i=0; i<p->G->nproc; i++) {
+    	int flag = 0;
+    	tmp_p = G->program_head->next;
+    	while(tmp_p!=NULL) {
+    		if (tmp_p->try_cpu_mask[i]==1) {
+    			flag = 1;
+    		}
+    		tmp_p = tmp_p->next;
+    	}
+    	if (flag==1) {
+    		//cpu i is idle core
+    		printf("cpu %d is a idle core\n", i);
+    	}
+    }
+
+    tmp_p = G->program_head->next;
+    while(tmp_p!=NULL) {
+    	pthread_spin_lock(&(tmp_p->cpu_cycle_status_lock));
     	reset_cpu_cycle_status(tmp_p);
     	pthread_spin_unlock(&(tmp_p->cpu_cycle_status_lock));
     	tmp_p = tmp_p->next;
