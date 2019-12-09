156a2af3e831f67bdaf07a5b6a841fe3be511510
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-12-09 13:54:51 -0600

    zhe

--- runtime/platform-main.c
+++ runtime/platform-main.c
@@ -94,7 +94,7 @@ int main(int argc, char* argv[]) {
         p->try_num_cpu = p->G->nproc-2;
         platform_activate_container(p);
         run_program(G, p);
-        for (j=0; i<2; j++) {
+        for (j=0; j<2; j++) {
             usleep(1000*1000); //to guarantee the correctness of the jump buffer: should be non-null (elastic_safe)
         }
     }
