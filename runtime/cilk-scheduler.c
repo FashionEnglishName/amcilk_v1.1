69ee785582043fccc6bd47c88daecaadfdc664c4
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-12-10 17:49:57 -0600

    zhe

--- runtime/cilk-scheduler.c
+++ runtime/cilk-scheduler.c
@@ -1535,33 +1535,6 @@ void worker_sleep_handling(__cilkrts_worker *w) {
                             }
                         } else { //deque is empty
                             if (__sync_bool_compare_and_swap(&(w->l->elastic_s), SLEEP_REQUESTED, SLEEPING_ADAPTING_DEQUE)) {
-                                deque_lock_self(w);
-                                Closure *cl;
-                                cl = deque_xtract_bottom(w, w->self);
-                                if (cl!=NULL) {
-                                    if (cl->status==CLOSURE_RETURNING) { //give up
-                                        w = __cilkrts_get_tls_worker();
-                                        cl = return_value(w, cl);
-                                        if (cl!=NULL) {
-                                            if (cl->status==CLOSURE_READY) {
-                                                deque_add_bottom(w, cl, w->self);
-                                                setup_for_execution(w, cl);
-                                                __sync_bool_compare_and_swap(&(w->l->elastic_s), SLEEPING_ADAPTING_DEQUE, SLEEP_REQUESTED);
-                                                w->exc = w->tail + DEFAULT_DEQ_DEPTH; //invoke exception handler
-                                                deque_unlock_self(w);
-                                                longjmp_to_user_code(w, cl);
-                                            } else {
-                                                printf("ERROR: error2 cl status %d\n", cl->status);
-                                                abort();
-                                            }
-                                        }
-                                    } else {
-                                        printf("ERROR: wrong cl status at bottom [%d] when deque is empty and go to sleep\n", cl->status);
-                                        abort();
-                                    }
-                                }
-                                deque_unlock_self(w);
-
                                 if (__sync_bool_compare_and_swap(&(w->l->elastic_s), SLEEPING_ADAPTING_DEQUE, SLEEPING_INACTIVE_DEQUE)) { 
                                     elastic_core_lock(w);
                                     w->g->elastic_core->ptr_sleeping_inactive_deque--;
